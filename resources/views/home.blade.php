f764e890bb2efea594d08d6516c39b36b478e2fc
Tarsis Lima <tarsis.lima@gmail.com>
2020-11-29 12:39:27 -0300

    prepara arquivo de entrada front

---
+++ resources/views/home.blade.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+        <link href="{{ mix('css/app.css') }}" type="text/css" rel="stylesheet"/>
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            /*.links > a {*/
+            /*    color: #636b6f;*/
+            /*    padding: 0 25px;*/
+            /*    font-size: 13px;*/
+            /*    font-weight: 600;*/
+            /*    letter-spacing: .1rem;*/
+            /*    text-decoration: none;*/
+            /*    text-transform: uppercase;*/
+            /*}*/
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
+    </head>
+    <body>
+        <div id="app" class="flex-center position-ref full-height">
+            <div class="content">
+              <user-register></user-register>
+            </div>
+        </div>
+        <script src="{{ mix('js/app.js') }}" type="text/javascript"></script>
+    </body>
+</html>
