4fe444ba2408ee6434d482418149a584ed1c458b
Tarsis Lima <tarsis.lima@gmail.com>
2020-11-30 00:27:18 -0300

    bootstrap app

---
+++ resources/js/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <div class="text-center" style="margin: 20px 0px 20px 0px;">
+
+      <img src="https://d8xabijtzlaac.cloudfront.net/Custom/Content/Themes/Shared/Images/marca-bemol.svg" width="100">
+      <h3 class="text-secondary">Omni App</h3>
+    </div>
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="collapse navbar-collapse">
+        <div class="navbar-nav">
+          <router-link to="/" class="nav-item nav-link">Inicio</router-link>
+        </div>
+      </div>
+    </nav>
+    <br/>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,29 @@
-require('./bootstrap');
+require('./bootstrap')
+window.Vue = require('vue')
+
+import App from './App.vue'
+import VueRouter from 'vue-router'
+import VueAxios from 'vue-axios'
+import axios from 'axios'
+import { routes } from './routes'
+import VueMask from 'v-mask'
+import { VueMaskDirective} from 'v-mask'
+
+Vue.use(VueMask)
+Vue.directive('mask', VueMaskDirective)
+
+Vue.use(VueRouter)
+Vue.use(VueAxios, axios)
+
+const router = new VueRouter({
+  mode: 'history',
+  routes: routes
+})
+
+const app = new Vue({
+  el: '#app',
+  router: router,
+  render: h => h(App),
+})
+
+
